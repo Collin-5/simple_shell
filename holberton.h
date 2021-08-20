b85928cf77e196ea85389ad691a615b731d104e7
Collin-5 <collinsotieno380@gmail.com>
2021-08-20 07:56:00 +0300

    strcpy/get_line

--- holberton.h
+++ holberton.h
@@ -33,5 +33,7 @@ void _exitSimpleShell(char **tokens, char *line);
 void ctrlc(int signum);
 int is_delim(char c, const char *delim);
 char *_strtok(char *src, const char *delim);
+char *_strcpy(char *dest, char *src);
+size_t get_line(char **str);
 
 #endif
